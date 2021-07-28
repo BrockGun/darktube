bea1c7947b57496fba0d0cdcf98c0e696d5dfa20
Brock <yles.94210@gmail.com>
2021-07-28 12:37:43 +0800

    新增點讚功能

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -17,21 +18,45 @@ class Video extends Model
 
     public function getThumbnailAttribute()
     {
-
         if($this->thumbnail_image) {
-
             return '/videos/'. $this->uid. '/'. $this->thumbnail_image;
-
         } else {
-
             return '/videos/default.png';
-
         }
+    }
 
+    public function getUploadedDateAttribute()
+    {
+        $d = new Carbon($this->created_at);
+        return $d->toFormattedDateString();
     }
 
     public function channel()
     {
         return $this->belongsTo(Channel::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+
+    public function dislikes()
+    {
+        return $this->hasMany(Dislike::class);
+    }
+
+    public function doesUserLikedVideo()
+    {
+        return $this->likes()
+        ->where('user_id' , auth()->id())
+        ->exists();
+    }
+
+    public function doesUserDislikedVideo()
+    {
+        return $this->dislikes()
+        ->where('user_id' , auth()->id())
+        ->exists();
     }
 }
--- resources/views/livewire/video/watch-video.blade.php
+++ resources/views/livewire/video/watch-video.blade.php
@@ -5,19 +5,39 @@
 
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-12">
-                <div class="video-container">
-                    <video controls preload="auto" id="yt-video" wire:ignore
-                           class="video-js vjs-16-9 vjs-styles=defaults vjs-big-play-centered" data-setup="{}" >
-                        <source src="{{ asset('videos/'. $video->uid. '/'. $video->processed_file) }}" type="application/x-mpegURL" />
-                        <p class="vjs-no-js">
-                            To view this video please enable JavaScript, and consider upgrading to a
-                            web browser that
-                            <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>                            >
-                        </p>
-                    </video>
+            <div class="col-md-12" wire:ignore>
+                <video controls preload="auto" id="yt-video"  class="video-js video-container vjs-styles=defaults vjs-big-play-centered" data-setup="{}" >
+                    <source src="{{ asset('videos/'. $video->uid. '/'. $video->processed_file) }}" type="application/x-mpegURL" />
+                    <p class="vjs-no-js">
+                        To view this video please enable JavaScript, and consider upgrading to a
+                        web browser that
+                        <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>                            >
+                    </p>
+                </video>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-8">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div>
+                                <h3>{{$video->title}}</h3>
+                                <p class="gray-text">{{$video->views}} views . {{$video->uploaded_date}}</p>
+                            </div>
+                            <div>
+                                <livewire:video.voting :video="$video"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-12"></div>
                 </div>
             </div>
+            <div class="col-md-4"></div>
         </div>
     </div>
 
@@ -25,7 +45,7 @@
         <script src="https://vjs.zencdn.net/7.11.4/video.min.js"></script>
 
         <script>
-            var player = videojs('yt-video',{height:490})
+            var player = videojs('yt-video')
             player.on('timeupdate', function () {
                 if(this.currentTime() > 3){
                     this.off('timeupdate')
